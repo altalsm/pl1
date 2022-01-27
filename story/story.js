f8b6a4c931145ba179adac84f145ee9940697369
altalsm <91274311+altalsm@users.noreply.github.com>
2022-01-27 11:21:49 -0500

    Update story.js

--- story/story.js
+++ story/story.js
@@ -2,7 +2,7 @@ let map;
 
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: 45.219784473869026, lng: 75.15426535537038  },
+    center: { lat: 45.219784473869026, lng: -75.15426535537038  },
     zoom: 8,
   });
 }
